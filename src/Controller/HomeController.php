feea72b34bb2f605b9b093cdb9081de8a3fc8374
Emory Mercera <ctrlemo@gmail.com>
2025-04-15 13:18:05 -0400

    create my first template for the Home (root) page

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/')]
+    public function index(): Response
+    {
+        $number = random_int(0, 100);
+
+        return $this->render('home/index.html.twig', [
+            'number' => $number,
+        ]);
+
+
+        // return new Response(
+        //     '<html lang="en"><body>Lucky number: '.$number.'</body></html>'
+        // );
+    }
+}
